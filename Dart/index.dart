99919705477b144edc861b7da962c51ddb4336f5
David Makinde <58576209+daevid-thegreat@users.noreply.github.com>
2023-01-25 09:28:18 +0100

    react-task

--- Dart/index.dart
+++ Dart/index.dart
@@ -1,3 +1,21 @@
-main(){
-  print("Hello World");
+import 'dart:io';
+
+main() {
+  stdout.writeln("What is your name: ?");
+  var name = stdin.readLineSync();
+  print("Hello $name");
 }
+
+// main() {
+//   var firstName = "David";
+//   String lastName = "Makinde";
+//   int age = 20;
+//   bool isMale = true;
+
+//   print("My name is $firstName $lastName and I am $age years old");
+//   if (isMale == true) {
+//     print("I am a boy");
+//   } else {
+//     print("I am a girl");
+//   }
+// }
---
+++ Javascript/React/react-task/src/components/Form.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Form = ({setInputText, todos, setTodos, inputText, setStatus}) => {
+  const inputTextHandler = (e) =>{
+    console.log(e.target.value);
+    setInputText(e.target.value)
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTodos([...todos, {text : inputText, completed : false, id:Math.random() * 1000}]);
+    setInputText("");
+  }
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  }
+
+    return (
+        <form>
+        <input type="text" value={inputText} onChange={inputTextHandler} className="todo-input" />
+        <button className="todo-button" onClick={submitHandler} type="submit">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+          <select onChange={statusHandler} name="todos" className="filter-todo">
+            <option value="all">All</option>
+            <option value="completed">Completed</option>
+            <option value="uncompleted">Uncompleted</option>
+          </select>
+        </div>
+      </form>
+    )
+}
+
+export default Form;
